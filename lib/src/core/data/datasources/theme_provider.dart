28da1a5c143d482942decab79aeec722439e80a5
Luqmanul Hakiem <luqmanul@ebconnect.com>
2025-02-10 18:51:39 +0700

    feat: theme mode toggle

---
+++ lib/src/core/data/datasources/theme_provider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  ThemeProvider() {
+    _loadThemeFromPrefs();
+  }
+
+  void toggleTheme(bool isDarkMode) {
+    _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    _saveThemeToPrefs();
+    notifyListeners();
+  }
+
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
+  Future<void> _saveThemeToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', _themeMode == ThemeMode.dark);
+  }
+
+  Future<bool> getThemePreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getBool('isDarkMode') ?? false;
+    return value;
+  }
+}
