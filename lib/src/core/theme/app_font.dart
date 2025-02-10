aede1e75d7bb58a47d5e83b4fad756ee74c4543e
Luqmanul Hakiem <luqmanul@ebconnect.com>
2025-02-10 16:45:28 +0700

    style: theme configuration

---
+++ lib/src/core/theme/app_font.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppFont {
+  static const TextStyle generalSansSerif = TextStyle(
+      fontFamily: "GeneralSans", fontWeight: FontWeight.w600, fontSize: 24);
+}
---
+++ lib/src/core/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightThemeMode = ThemeData.light().copyWith();
+  static final darkThemeMode = ThemeData.dark().copyWith();
+}
